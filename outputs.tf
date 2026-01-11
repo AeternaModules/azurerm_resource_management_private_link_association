30a6147b9e32ecb440133821cd48514451274499
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:59:53 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "resource_management_private_link_associations" {
+  description = "All resource_management_private_link_association resources"
+  value       = azurerm_resource_management_private_link_association.resource_management_private_link_associations
+}
+
