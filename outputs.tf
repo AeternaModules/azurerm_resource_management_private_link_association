746bfcdd2633701ed990bc5c4421f8d3786c0639
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:00 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "resource_management_private_link_associations" {
-  description = "All resource_management_private_link_association resources"
-  value       = azurerm_resource_management_private_link_association.resource_management_private_link_associations
-}
 output "resource_management_private_link_associations_management_group_id" {
-  description = "List of management_group_id values across all resource_management_private_link_associations"
-  value       = [for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : v.management_group_id]
+  description = "Map of management_group_id values across all resource_management_private_link_associations, keyed the same as var.resource_management_private_link_associations"
+  value       = { for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : k => v.management_group_id }
 }
 output "resource_management_private_link_associations_name" {
-  description = "List of name values across all resource_management_private_link_associations"
-  value       = [for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : v.name]
+  description = "Map of name values across all resource_management_private_link_associations, keyed the same as var.resource_management_private_link_associations"
+  value       = { for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : k => v.name }
 }
 output "resource_management_private_link_associations_public_network_access_enabled" {
-  description = "List of public_network_access_enabled values across all resource_management_private_link_associations"
-  value       = [for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : v.public_network_access_enabled]
+  description = "Map of public_network_access_enabled values across all resource_management_private_link_associations, keyed the same as var.resource_management_private_link_associations"
+  value       = { for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : k => v.public_network_access_enabled }
 }
 output "resource_management_private_link_associations_resource_management_private_link_id" {
-  description = "List of resource_management_private_link_id values across all resource_management_private_link_associations"
-  value       = [for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : v.resource_management_private_link_id]
+  description = "Map of resource_management_private_link_id values across all resource_management_private_link_associations, keyed the same as var.resource_management_private_link_associations"
+  value       = { for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : k => v.resource_management_private_link_id }
 }
 output "resource_management_private_link_associations_tenant_id" {
-  description = "List of tenant_id values across all resource_management_private_link_associations"
-  value       = [for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : v.tenant_id]
+  description = "Map of tenant_id values across all resource_management_private_link_associations, keyed the same as var.resource_management_private_link_associations"
+  value       = { for k, v in azurerm_resource_management_private_link_association.resource_management_private_link_associations : k => v.tenant_id }
 }
 
